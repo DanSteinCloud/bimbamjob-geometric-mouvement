ed698938175cd04afbb2849c507374db5a3bbb82
DanielStein <daniel.cloudware@gmail.com>
2024-09-06 10:28:56 -0700

    ajout des coordonnes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import "./App.css";
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import Grille from "./components/Grille";
 import FICHIER_DE_COMMANDE_TONDEUSE from "./datasource/fichier.txt";
 
 export default function App() {
+  const position = useRef(null);
   const [order, setOrder] = useState([]);
+  const [showCoordinate, setShowCoordinate] = useState(false);
+  const [newIndex, setNewIndex] = useState();
   const [data, setData] = useState();
   const [isDeactivating, setIsDeactivating] = useState(false);
   const surface = [
     [1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1 ,1],
     [1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1],
+  ];
+  const coordinates = [
+    [0, 0], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5],
+    [1, 0], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5],
+    [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [2, 5],
+    [3, 0], [3, 1], [3, 2], [3, 3], [3, 4], [3, 5],
   ];
   
   const tondreSurfaceParGrille = () => {
@@ -32,14 +43,101 @@ export default function App() {
   };
 
   const couvrirSurfaceDeVerdure = (index) => {
+    setNewIndex(index);
     const newOrder = [...order, index];
     setOrder(newOrder);
+    position.current = affectCoordinates(index);
+    console.log(position.current)
+    setShowCoordinate(true);
     // deactivate
-    if (newOrder.length === surface.flat(1).filter(Boolean).length) {
-      tondreSurfaceParGrille();
-    }
+    if (newOrder.length === surface.flat().filter(Boolean).length) {
+      tondreSurfaceParGrille();   
+    }  
   };
+  
+  const affectCoordinates = (index) => {
+    switch(index) {
+      case 0:
+        return [0, 5];
+      case 1:
+        return [1, 5];
+      case 2:
+        return [2, 5];
+      case 3:
+        return [3, 5];
+      case 4:
+        return [4, 5];
+      case 5:
+        return [5, 5];
 
+      case 6:
+        return [0, 4];
+      case 7:
+        return [1, 4];
+      case 8:
+        return [2, 4];
+      case 9:
+        return [3, 4];
+      case 10:
+        return [4, 4];
+      case 11:
+        return [5, 4];
+
+      case 12:
+        return [0, 3];
+      case 13:
+        return [1, 3];
+      case 14:
+        return [2, 3];
+      case 15:
+        return [3, 3];
+      case 16:
+        return [4, 3];
+      case 17:
+        return [5, 3];
+
+      case 18:
+        return [0, 2];
+      case 19:
+        return [1, 2];
+      case 20:
+        return [2, 2];
+      case 21:
+        return [3, 2];
+      case 22:
+        return [4, 2];
+      case 23:
+        return [5, 2];
+
+      case 24:
+        return [0, 1];
+      case 25:
+        return [1, 1];
+      case 26:
+        return [2, 1];
+      case 27:
+        return [3, 1];
+      case 28:
+        return [4, 1];
+      case 29:
+        return [5, 1];
+
+      case 30:
+        return [0, 0];
+      case 31:
+        return [1, 0];
+      case 32:
+        return [2, 0];
+      case 33:
+        return [3, 0];
+      case 34:
+        return [4, 0];
+      case 35:
+        return [5, 0];
+      default:
+        return [0, 0];
+    }
+  }
   useEffect(() => {
     fetch(FICHIER_DE_COMMANDE_TONDEUSE)
       .then(function(response){
@@ -50,6 +148,10 @@ export default function App() {
       })
   });
 
+  useEffect(() => {
+    position.current = affectCoordinates(newIndex);
+  }, [newIndex]);
+
   return (
     <div className="wrapper">
       <div
@@ -58,13 +160,15 @@ export default function App() {
           gridTemplateColumns: `repeat(${surface[0].length}, 1fr)`,
         }}
       >
-        {surface.flat(1).map((value, index) => {
+        {surface.flat().map((value, index) => {
           return value ? (
             <Grille
               key={index}
               label={`Cell ${index}`}
               filled={order.includes(index)}
-              onClick={() => couvrirSurfaceDeVerdure(index)}
+              onClick={() => {couvrirSurfaceDeVerdure(index)}}
+              coordinate={position.current}
+              showCoordinate={showCoordinate}
               isDisabled={order.includes(index) || isDeactivating}
             />
           ) : (
@@ -74,7 +178,7 @@ export default function App() {
       </div>
       <div>
       <p>Cliquer du bas vers le haut, de la gauche vers la droite, pour recouvrir la surface à tondre</p>
-      <p>La tondeuse demarre une fois la dernièrre grille cevouverte</p>
+      <p>La tondeuse demarre une fois la dernièrre grille revouverte</p>
       </div>
       
     </div>
--- src/components/Grille.js
+++ src/components/Grille.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-export default function Grille({filled, onClick, isDisabled, label}) {
+export default function Grille({filled, onClick, isDisabled, label, showCoordinate, coordinate}) {
  return (
    <button
       type="button"
+      color="white"
       aria-label={label}
       disabled={isDisabled}
       onClick={onClick}
       className={filled ? "cell cell-activated" : "cell"}
-   />
+   >{showCoordinate ? `(` + coordinate[0] + `,` + coordinate[1] + `)`: ''}
+   </button>
 );
 }
